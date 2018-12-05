68d147c7ec417397679b96162f612717622c8bd1
Martin Giannechini <martingiannechini@gmail.com>
2018-12-05 13:44:49 -0300

    Remove cors package

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const proxy = require("express-http-proxy");
-const cors = require("cors");
 const app = require("express")();
 const argv = require("yargs").argv;
 
@@ -7,7 +6,6 @@ if (!argv.url) throw new Error("Must pass a url param");
 
 const PORT = argv.port || 8080;
 
-app.use(cors());
 app.all("*", proxy(argv.url));
 
 app.listen(PORT, () => {
