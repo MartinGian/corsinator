3be11c6ddb3d6a26bc96a5af4a531e240436b014
Martin Giannechini <martingiannechini@gmail.com>
2018-12-18 10:28:17 -0300

    Add cors

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const proxy = require("express-http-proxy");
 const app = require("express")();
 const argv = require("yargs").argv;
+const cors = require("cors");
 
 if (!argv.url) throw new Error("Must pass a url param");
 
 const PORT = argv.port || 8080;
+
+app.use(cors());
 
 app.all("*", proxy(argv.url));
 
